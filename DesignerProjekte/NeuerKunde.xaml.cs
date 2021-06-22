73435237ac85c66d55f6e13415748f95b7f224b1
Michael Beck <mbeck@lutz-und-grub.de>
2021-06-22 11:28:48 +0200

    DesignerProjekte

---
+++ DesignerProjekte/NeuerKunde.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DesignerProjekte
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void startGridContainerWindow_Click(object sender, RoutedEventArgs e)
+        {
+            //MessageBox.Show("Grid Container wird geöffnet ...");
+            GridContainer gcFenster = new GridContainer();
+            gcFenster.WindowStartupLocation = WindowStartupLocation.Manual;
+            gcFenster.Top = 10;
+            gcFenster.Left = 500;
+            gcFenster.ShowDialog();
+        }
+
+        private void startUniGridContainerWindow_Click(object sender, RoutedEventArgs e)
+        {
+            UniformGridFenster ufgFenster = new UniformGridFenster();
+            ufgFenster.Show();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+    }
+}
