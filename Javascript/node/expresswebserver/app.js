f975f52157da248461cbdf367e915886c4c07a3a
michael beck <michael.beck@training.lug-ag.de>
2021-07-13 08:56:52 +0200

    express post route added

--- Javascript/node/expresswebserver/app.js
+++ Javascript/node/expresswebserver/app.js
@@ -1,6 +1,8 @@
 const { response } = require("express");
 let express = require("express");
 let app = express();
+let formidable = require('formidable');
+
 let port = 8888;
 
 app.use(express.static(__dirname + "/public"));
@@ -14,7 +16,19 @@ app.get('/SubmitBenutzername',function(req,res){
     res.writeHead(200, {'Content-Type':'text/html'});
     res.write("Hello " + req.query.benutzername + "!");
     res.end(" Have a great day!");
-    console.log("Handled request from " + req.query.benutzername);
+    console.log("Handled GET request from " + req.query.benutzername);
+});
+
+
+//HTTP-POST Handling
+app.post('/PostBenutzername',function(req,res){
+    let form = new formidable.IncomingForm();
+    form.parse(req, function(err, fields){
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write("Hello " + fields.benutzername + "!<br>");
+        res.end("Have a POST great day!");
+        console.log("Handled POST request from " + fields.benutzername);
+    }); 
 });
 
 app.listen(port);
